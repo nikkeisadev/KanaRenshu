5a0b9881ed50ebf4812f283013fdb4ec70700cee
nikkeisadev <137056695+nikkeisadev@users.noreply.github.com>
2023-10-19 12:58:39 +0200

    Complete update of the UI 🎀

--- source/KanaRenshu.py
+++ source/KanaRenshu.py
@@ -9,7 +9,7 @@ from PIL import Image
 #Basic variables, and all Hiragana Character in one String.
 answerIsRight = True
 hiragana_chart = "あいうえおかがきぎくぐけげこごさざしじすずせぜそぞただちぢつづてでとどなにぬねのはばぱひびぴふぶぷへべぺほぼぽまみむめもやゆよらりるれろわゐゑをんゔ" 
-katakana_chart = 'アイウエオカガキギクグケゲコゴサザシジスズセゼソゾタダチヂツヅテデトドナニヌネノハバパヒビピフブプヘベペホボポマミムメモヤユヨラリルレロワヰヱヲンヴヷヸヹヺ"
+katakana_chart = "アイウエオカガキギクグケゲコゴサザシジスズセゼソゾタダチヂツヅテデトドナニヌネノハバパヒビピフブプヘベペホボポマミムメモヤユヨラリルレロワヰヱヲンヴヷヸヹヺ"
 charFormat = pykakasi.kakasi()
 customtkinter.set_appearance_mode("dark")
 consoleDebug = '[CONSOLE]> '
@@ -40,11 +40,11 @@ class KanaRenshu(customtkinter.CTk):
                                                size=(self.width, self.height))
         self.bg_image_label = customtkinter.CTkLabel(self, image=self.bg_image)
         self.bg_image_label.grid(row=0, column=0)
-        self.text_label = customtkinter.CTkButton(self, font=("Arial", 55, 'bold'), text_color='#FFCBDE', text=selectedChar, width=370, height=50, fg_color=("#4B4648", "#4B4648"))
+        self.text_label = customtkinter.CTkButton(self, font=("Arial", 55, 'bold'), text_color='#938CD9', bg_color="#938CD9", text=selectedChar, width=370, height=50, fg_color=("#4B4648", "#4B4648"), corner_radius=(30))
         self.text_label.grid(row=0, column=0, padx=30, pady=(0, 45))
-        self.check_button = customtkinter.CTkButton(self, font=("Arial", 10, 'bold'), text_color='#FFCBDE', text="Check ✔", width=370, height=10, fg_color=("#4B4648", "#4B4648"), command=self.checkUserInput)
+        self.check_button = customtkinter.CTkButton(self, font=("Arial", 10, 'bold'), text_color='#938CD9', bg_color="#938CD9", text="Check ✔", width=370, height=10, fg_color=("#4B4648", "#4B4648"), command=self.checkUserInput, corner_radius=(50))
         self.check_button.grid(row=0, column=0, padx=30, pady=(46, 0))
-        self.username_entry = customtkinter.CTkEntry(self, justify='center', text_color='#4B4648', font=("Arial", 13, 'bold'),  width=370, height=29, placeholder_text="Type the character in romaji: ")
+        self.username_entry = customtkinter.CTkEntry(self, justify='center', text_color='#938CD9', bg_color="#938CD9", font=("Arial", 13, 'bold'), fg_color=("#4B4648", "#4B4648"),  width=370, height=29, placeholder_text="Type the character in romaji: ", corner_radius=(30))
         self.username_entry.grid(row=0, column=0, padx=30, pady=(98, 1))
     
     #Checking the answer.
@@ -56,19 +56,20 @@ class KanaRenshu(customtkinter.CTk):
         if self.translatedChar == userInput.lower():
             
             print(f'{consoleDebug}Correct!')
-            self.text_label = customtkinter.CTkButton(self, font=("Arial", 56, 'bold'), text_color='#FFCBDE', text="✔", width=370, height=50, fg_color=("#4B4648", "#4B4648"))
+            self.text_label = customtkinter.CTkButton(self, font=("Arial", 56, 'bold'), text_color='#938CD9', text="✔", width=370, height=50, fg_color=("#4B4648", "#4B4648"))
             
             os.system('cls')
             selectedChar = random.choice(hiragana_chart)
             result = charFormat.convert(selectedChar)[0]
 
             self.translatedChar = format(result['hepburn'])
-            self.text_label = customtkinter.CTkButton(self, font=("Arial", 55, 'bold'), text_color='#FFCBDE', text=selectedChar, width=370, height=50, fg_color=("#4B4648", "#4B4648"))
+            self.text_label = customtkinter.CTkButton(self, font=("Arial", 55, 'bold'), text_color='#938CD9', bg_color="#938CD9", text=selectedChar, width=370, height=50, fg_color=("#4B4648", "#4B4648"), corner_radius=(30))
             self.text_label.grid(row=0, column=0, padx=30, pady=(0, 45))
-            
-            self.username_entry = customtkinter.CTkEntry(self, justify='center', text_color='#4B4648', font=("Arial", 13, 'bold'),  width=370, height=29, placeholder_text="Type the character in romaji: ")
+            self.check_button = customtkinter.CTkButton(self, font=("Arial", 10, 'bold'), text_color='#938CD9', bg_color="#938CD9", text="Check ✔", width=370, height=10, fg_color=("#4B4648", "#4B4648"), command=self.checkUserInput, corner_radius=(50))
+            self.check_button.grid(row=0, column=0, padx=30, pady=(46, 0))
+            self.username_entry = customtkinter.CTkEntry(self, justify='center', text_color='#938CD9', bg_color="#938CD9", font=("Arial", 13, 'bold'), fg_color=("#4B4648", "#4B4648"),  width=370, height=29, placeholder_text="Type the character in romaji: ", corner_radius=(30))
             self.username_entry.grid(row=0, column=0, padx=30, pady=(98, 1))
-    
+        
         else:
             os.system('cls')
             selectedChar = random.choice(hiragana_chart)
@@ -77,13 +78,14 @@ class KanaRenshu(customtkinter.CTk):
             self.translatedChar = format(result['hepburn'])
             
             print(f'{consoleDebug}Wrong!')
-            
-            self.text_label = customtkinter.CTkButton(self, font=("Arial", 55, 'bold'), text_color='#FFCBDE', text=selectedChar, width=370, height=50, fg_color=("#ff3d3d", "#ff3d3d"))
+            self.text_label = customtkinter.CTkButton(self, font=("Arial", 55, 'bold'), text_color='#938CD9', bg_color="#938CD9", text=selectedChar, width=370, height=50, fg_color=("#765793", "#765793"), corner_radius=(30))
             self.text_label.grid(row=0, column=0, padx=30, pady=(0, 45))
-            
-            self.username_entry = customtkinter.CTkEntry(self, justify='center', text_color='#4B4648', font=("Arial", 13, 'bold'),  width=370, height=29, placeholder_text="Type the character in romaji: ")
+            self.check_button = customtkinter.CTkButton(self, font=("Arial", 10, 'bold'), text_color='#938CD9', bg_color="#938CD9", text="Check ✔", width=370, height=10, fg_color=("#765793", "#765793"), command=self.checkUserInput, corner_radius=(50))
+            self.check_button.grid(row=0, column=0, padx=30, pady=(46, 0))
+            self.username_entry = customtkinter.CTkEntry(self, justify='center', text_color='#938CD9', bg_color="#938CD9", font=("Arial", 13, 'bold'), fg_color=("#4B4648", "#4B4648"),  width=370, height=29, placeholder_text="Wrong romaji!", corner_radius=(30))
             self.username_entry.grid(row=0, column=0, padx=30, pady=(98, 1))
 
 if __name__ == "__main__":
+    
     app = KanaRenshu()
     app.mainloop()
